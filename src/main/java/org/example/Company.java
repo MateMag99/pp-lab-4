6d66ee31a391a78a46d6d9534e2e4006f921e73f
MateMag99 <matemag99@gmail.com>
2024-04-07 18:04:06 +0200

    zad2

--- src/main/java/org/example/Company.java
+++ src/main/java/org/example/Company.java
@@ -1,20 +1,22 @@
 package org.example;
 
-import com.sun.security.jgss.GSSUtil;
+import org.example.employees.Employee;
+import org.example.employees.Manager;
+import org.example.employees.Worker;
 
 public class Company {
 
     public static void main(String[] args) {
         Employee[] employees = new Employee[5];
-        employees[0] = new Employee("Jamal Kingstone", 500);
-        employees[1] = new Employee("Bartosz Walaszek", 1000);
+        employees[0] = new Manager("Jamal Kingstone", 500, 0);
+        employees[1] = new Worker("Bartosz Walaszek", 1000, "");
         employees[2] = new Employee("Bob Marley", 1500);
         employees[3] = new Employee("Snoop Dog", 2000);
-        employees[4] = new Employee("Wiz Khalifa", 2500);
+        employees[4] = new Worker("Wiz Khalifa", 2500, "");
 
-        System.out.println(employees[2]);
-        employees[2].setSalary(420);
-        System.out.println(employees[2]);
+//        System.out.println(employees[2]);
+//        employees[2].setSalary(420);
+//        System.out.println(employees[2]);
 
         for (int i = 0; i < employees.length; i++) {
             System.out.println(employees[i]);
